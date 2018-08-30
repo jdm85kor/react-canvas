41a2aff400853fc201510709d5b28b2df7d9a8fb
anthony <anthony@mistock170615ui-MacBook-Pro.local>
2018-08-30 11:47:17 +0900

    feat: add handle method in App comp

--- src/App.js
+++ src/App.js
@@ -2,14 +2,57 @@ import React, { Component } from 'react';
 import Canvas from './Canvas';
 import ControlPanel from './ControlPanel';
 
-class App extends Component {
+export default class App extends Component {
 constructor(props) {
     super(props);
+    this.state = {
+      width: 500,
+      height: 500,
+      colorOption: 'black',
+    };
+
+    this.handleChangeColorOption = this.handleChangeColorOption.bind(this);
+    this.handleClickMerge = this.handleClickMerge.bind(this);
+    this.handleClickClear = this.handleClickClear.bind(this);
+  }
+
+  handleChangeColorOption(e) {
+    // e.preventDefault();
+    this.setState({
+      colorOption: e.target.value,
+    });
+  }
+
+  handleClickMerge(e) {
+    e.preventDefault();
+    this.canvasRef.merge();
+  }
+
+  handleClickClear(e) {
+    e.preventDefault();
+    this.canvasRef.clear();
   }
 
   render() {
-    return ;
+    const style = {
+      width: '800px',
+      height: '600px',
+    }
+    return (
+      <div style={style}>
+        <Canvas
+          ref={(Canvas) => { this.canvasRef = Canvas }}
+          width={this.state.width}
+          height={this.state.height}
+          colorOption={this.state.colorOption}
+        />
+        <ControlPanel
+          colorOption={this.state.colorOption}
+          handleChangeColorOption={this.handleChangeColorOption}
+          handleClickMerge={this.handleClickMerge}
+          handleClickClear={this.handleClickClear}
+        />
+      </div>
+    );
   }
 };
-
-export default App;
--- src/Canvas.js
+++ src/Canvas.js
@@ -5,10 +5,19 @@ import { findDOMNode } from 'react-dom'
 export default class Canvas extends Component {
   constructor(props) {
     super(props);
+
+    this.merge = this.merge.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  merge() {
+  }
+
+  clear() {
   }
 
   render() {
-    return ;
+    return ();
   }
 };
 
